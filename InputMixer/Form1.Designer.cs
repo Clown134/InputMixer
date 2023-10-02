acbc16404a3bbe7bd296750502b004561cb79ace
chigirev <chigirev@LABI>
2023-10-02 16:54:07 +0500

    Добавьте файлы проекта.

---
+++ InputMixer/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace InputMixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AllowDrop = true;
+            this.trackBar1.Location = new System.Drawing.Point(12, 36);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(213, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Tag = "0";
+            this.trackBar1.Value = 100;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(230, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(38, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Image = global::InputMixer.Properties.Resources.VolumePic;
+            this.checkBox1.Location = new System.Drawing.Point(230, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(38, 38);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Tag = "0";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 522);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Микшер ввода";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ InputMixer/Form1.cs
@@ -0,0 +1,190 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using NAudio.Mixer;
+using NAudio.Wave;
+using NAudio.Wasapi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Windows.Forms;
+using InputMixer.Properties;
+using System.Drawing;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace InputMixer
+{
+    public partial class Form1 : Form
+    {
+        List<TrackBar> trackBarList;
+        List<Label> labels;
+        List<CheckBox> checkBoxes;
+        List<TextBox> textBoxList;
+        const int countLine = 6;    //константа, количество строк
+
+        public Form1()
+        {
+            InitializeComponent();
+            ListsAdd();
+            if (trackBarList.Count >= countLine)
+                this.Height = trackBarList[countLine - 1].Location.Y + trackBarList[countLine - 1].Size.Height + 50;
+            else
+                this.Height = trackBarList[trackBarList.Count - 1].Location.Y + trackBarList[trackBarList.Count - 1].Size.Height + 50;
+            this.Width = trackBarList[trackBarList.Count - 1].Location.X + trackBarList[trackBarList.Count - 1].Size.Width + 90;
+
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Size.Width-this.Width, Screen.PrimaryScreen.Bounds.Size.Height-this.Height-40);
+
+            notifyIcon1.ContextMenu = new ContextMenu(
+            new[]
+            {
+                new MenuItem("Show form", (s, e) => Show()),
+                new MenuItem("Hide form", (s, e) => Hide()),
+                new MenuItem("Exit", (s, e) => {notifyIcon1.Dispose(); Environment.Exit(0);} ),
+            }) ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.ShowInTaskbar = false;
+            if (WaveIn.DeviceCount == 0)
+            {
+                MessageBox.Show("Устройств ввода не найдено");
+                Close();
+            }
+            for (int i = 1;i< WaveIn.DeviceCount; i++)
+            {
+                this.Controls.Add(trackBarList[i]);
+                this.Controls.Add(labels[i]);
+                this.Controls.Add(checkBoxes[i]);
+                this.Controls.Add(textBoxList[i]);
+            }
+            ScanSoundCards();
+            SetTrackBars();
+        }
+
+        private void ListsAdd()
+        {
+            trackBarList = new List<TrackBar>();
+            labels = new List<Label>();
+            checkBoxes = new List<CheckBox>();
+            textBoxList = new List<TextBox>();
+            trackBarList.Add(trackBar1);
+            labels.Add(label1);
+            checkBoxes.Add(checkBox1);
+            textBoxList.Add(textBox1);
+
+            for (int i = 1; i < WaveIn.DeviceCount; i++)
+            {
+                trackBarList.Add(new TrackBar());
+                trackBarList[i].Size = new Size(213, 45);
+                if ((trackBarList.Count - 1) % (countLine) != 0)
+                    trackBarList[i].Location = new Point(trackBarList[i - 1].Location.X, trackBarList[i - 1].Location.Y + 80);
+                else
+                    trackBarList[i].Location = new Point(trackBarList[trackBarList.Count - countLine - 1].Location.X + 320, trackBarList[trackBarList.Count - countLine - 1].Location.Y);
+                trackBarList[i].Tag = Convert.ToInt32(trackBarList[i - 1].Tag) + 1;
+                trackBarList[i].Maximum = 100;
+                trackBarList[i].Scroll += trackBar1_Scroll;
+                
+                labels.Add(new Label());
+                if ((labels.Count - 1) % (countLine) != 0)
+                    labels[i].Location = new Point(labels[i - 1].Location.X, labels[i - 1].Location.Y + 80);
+                else
+                    labels[i].Location = new Point(labels[labels.Count - countLine - 1].Location.X + 320, labels[labels.Count - countLine - 1].Location.Y);
+
+                checkBoxes.Add(new CheckBox());
+                checkBoxes[i].Appearance = Appearance.Button;
+                checkBoxes[i].AutoSize = true;
+                if ((checkBoxes.Count - 1) % (countLine) != 0)
+                    checkBoxes[i].Location = new Point(checkBoxes[i - 1].Location.X, checkBoxes[i - 1].Location.Y + 80);
+                else
+                    checkBoxes[i].Location = new Point(checkBoxes[checkBoxes.Count - countLine - 1].Location.X + 320, checkBoxes[checkBoxes.Count - countLine - 1].Location.Y);
+                checkBoxes[i].CheckedChanged += checkBox1_CheckedChanged;
+                checkBoxes[i].Tag = Convert.ToInt32(checkBoxes[i - 1].Tag) + 1;
+
+                textBoxList.Add(new TextBox());
+                textBoxList[i].Cursor = Cursors.Default;
+                textBoxList[i].Size = textBoxList[0].Size;
+                if ((textBoxList.Count - 1) % (countLine) != 0)
+                    textBoxList[i].Location = new Point(textBoxList[i - 1].Location.X, textBoxList[i - 1].Location.Y + 80);
+                else
+                    textBoxList[i].Location = new  Point(textBoxList[textBoxList.Count - countLine - 1].Location.X + 320, textBoxList[textBoxList.Count - countLine - 1].Location.Y);
+                textBoxList[i].Tag = Convert.ToInt32(textBoxList[i - 1].Tag) + 1;
+            }
+        }
+
+        private void ScanSoundCards()
+        {
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                labels[i].Text = WaveIn.GetCapabilities(i).ProductName;
+                labels[i].Visible = true;
+                trackBarList[i].Visible = true;
+                checkBoxes[i].Visible = true;
+            }
+        }
+
+        private void SetTrackBars()
+        {
+            for (int i = 0; i < WaveIn.DeviceCount; i++)
+            {
+                var waveInEvent = new WaveInEvent() { DeviceNumber = i };
+                var device = waveInEvent.GetMixerLine();
+                var volumeControl = device.Controls.FirstOrDefault(x => x.ControlType == MixerControlType.Volume) as UnsignedMixerControl;
+                trackBarList[i].Value = Convert.ToInt32(volumeControl.Percent);
+            }
+            for (int i = 0;i < WaveIn.DeviceCount; i++)
+            {
+                var waveInEvent = new WaveInEvent() { DeviceNumber = i };
+                var device = waveInEvent.GetMixerLine();
+                var volumeControl = device.Controls.FirstOrDefault(x => x.ControlType == MixerControlType.Mute) as BooleanMixerControl;
+                checkBoxes[i].Checked = volumeControl.Value;
+                if (checkBoxes[i].Checked)
+                    checkBoxes[i].Image = Resources.NoVolumePic;
+                else
+                    checkBoxes[i].Image = Resources.VolumePic;
+            }
+            for (int i = 0; i < trackBarList.Count; i++)
+                textBoxList[i].Text = trackBarList[i].Value.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            var trackBar = sender as TrackBar;
+            var waveInEvent = new WaveInEvent() { DeviceNumber = Convert.ToInt32(trackBar.Tag)};
+            var device = waveInEvent.GetMixerLine();
+            var volumeControl = device.Controls.FirstOrDefault(x => x.ControlType==MixerControlType.Volume) as UnsignedMixerControl;
+            volumeControl.Percent = trackBar.Value;
+            textBoxList[Convert.ToInt32(trackBar.Tag)].Text = trackBar.Value.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+            var waveInEvent = new WaveInEvent() { DeviceNumber = Convert.ToInt32(checkBox.Tag) };
+            var device = waveInEvent.GetMixerLine();
+            var volumeControl = device.Controls.FirstOrDefault(x => x.ControlType == MixerControlType.Mute) as BooleanMixerControl;
+            volumeControl.Value = checkBox.Checked;
+            if (checkBox.Checked)
+                checkBox.Image = Resources.NoVolumePic;
+            else
+                checkBox.Image = Resources.VolumePic;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //WindowState = FormWindowState.Normal;
+            Show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hide();
+            this.ShowInTaskbar = false;
+            e.Cancel = true;
+        }
+    }
+}
